ab3999377d83cfe853a12013d26fb73038923ca0
Aaditya Bhatia <aadityabhatia@gmail.com>
2023-06-26 18:41:51 -0400

    initial framework

---
+++ README.md
@@ -0,0 +1,41 @@
+# Documentation Website
+
+Built using [Docusaurus 2](https://docusaurus.io/).
+
+To update content, make changes under the [docs](docs) directory, commit, and push to the main branch.
+
+### Setup
+
+```
+$ npm ci
+```
+
+### Local Development
+
+```
+$ npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build static bundle
+
+```
+$ npm run build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Test locally built static bundle
+
+```
+$ npm run serve
+```
+
+### Deployment
+
+Though this is done automatically using GitHub Actions, use the following command to deploy an update to the `gh-pages` branch manually.
+
+```
+$ npm run deploy
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,92 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Generally Genius Bot Framework',
+  tagline: 'A generals.io distributed bot framework implementing modular strategies',
+  favicon: 'img/logo.png',
+
+  url: 'https://corsaircoalition.github.io',
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  organizationName: 'corsaircoalition',
+  projectName: 'corsaircoalition.github.io',
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful metadata like html lang
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ['@docusaurus/theme-mermaid'],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/generals-io-card.jpg',
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'CorsairCoalition',
+        logo: {
+          alt: 'Corsair Coalition',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            to: 'introduction',
+            position: 'left',
+            label: 'Introduction',
+          },
+          {
+            href: 'https://github.com/CorsairCoalition/docs#readme',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Corsair Coalition. All Rights Reserved.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
